dd2ef53bfc861c661f94e00312a3cdfe16ccf4d9
Dato Kvirikashvili <kvirika137@gmail.com>
2021-07-02 04:51:18 +0400

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '@/assets/scss/main.scss';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  font-family: $Helvetica_normal, $Helvetica_it,
+  $Helvetica_roman, $Helvetica_it_56, $Helvetica_bold, $Helvetica_it_bold;
+
 }
 </style>
